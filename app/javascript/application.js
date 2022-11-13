b3f05d91744eedab4679a7a19ccdb0f3ff910d7d
PEDRO DUARTE <pedrobradu@gmail.com>
2022-11-13 13:10:26 -0300

    botão de conect com meta mask instalado

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+// async function connect(){
+//   if (typeof window.ethereum !== "undefined"){
+//     await window.ethereum.request({method: "eth_requestAccounts"})
+//   }
+// }
